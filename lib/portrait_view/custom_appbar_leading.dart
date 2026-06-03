fe8b02e2777e2c660fb460383e3707f17e93a9c5
AfalpHy <736353503@qq.com>
2026-06-03 12:13:19 +0800

    fix: drawer failed to open on ios

--- lib/portrait_view/custom_appbar_leading.dart
+++ lib/portrait_view/custom_appbar_leading.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:sylvakru/layer/layers_manager.dart';
 import 'package:sylvakru/portrait_view/portrait_view.dart';
@@ -6,7 +8,9 @@ Widget customAppBarLeading(BuildContext context, {String label = ''}) {
   return IconButton(
     icon: Icon(label.isEmpty ? Icons.menu : Icons.arrow_back_ios_new_rounded),
     onPressed: () => label.isEmpty
-        ? portraitKey.currentState?.openDrawer()
+        ? Platform.isIOS
+              ? portraitKey.currentState?.openEndDrawer()
+              : portraitKey.currentState?.openDrawer()
         : layersManager.popDetail(label),
   );
 }
