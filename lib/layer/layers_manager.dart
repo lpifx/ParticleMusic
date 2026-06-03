9f8b24aa05e13a9b767578aaf216315a8a9b29dc
AfalpHy <736353503@qq.com>
2026-06-04 07:50:06 +0800

    trivial

--- lib/layer/layers_manager.dart
+++ lib/layer/layers_manager.dart
@@ -238,7 +238,7 @@ class LayersManager {
 
     detailWidgetMap[rootLayer] = detailLayer;
 
-    layersManager.updateBackground();
+    await layersManager.updateBackground();
 
     visibleNotifier.value = false;
     final detailPage = createPage(detailLayer);
@@ -334,7 +334,7 @@ class LayersManager {
       visibleNotifier = settingsVisibleNotifier;
     }
 
-    layersManager.updateBackground();
+    await layersManager.updateBackground();
 
     visibleNotifier.value = true;
     if (rootKey.currentState?.canPop() ?? false) {
